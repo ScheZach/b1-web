26a586ceaf4ce1f63e406d72b25cfce1bab6725c
ZSlongtalk <californiazds@gmail.com>
2016-09-27 09:09:46 -0600

    Add files via upload

--- myself.js
+++ myself.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
 	$("#myselfinfo").hide();
+	$("#myselfvideo").hide();
+	$("#music").hide();
 	$("#myselfbutton").click (function() {
 		$("#myselfinfo").slideToggle(300);
+		$("#myselfvideo").slideToggle(300);
 	});
-	
-	
-	
+	$("#musicbutton").click (function() {
+		$("#music").slideToggle(300);
+	});
+	$(".mousechange").mouseenter (function() {
+		$(this).css("font-size", "2.5em");
+		$(this).css("color", "#0000FF");
+	});
+	$(".mousechange").mouseleave (function() {
+		$(this).css("font-size", "2em");
+		$(this).css("color", "#000000");
+	});
 	
 	
 	
